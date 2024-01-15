958acc7de96d9e50265e01e9377e4e8fe61ea5de
Samme Janderson <samme.san@gmail.com>
2024-01-15 07:47:25 -0300

    keybinds

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,2 +1,10 @@
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move command
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- paste without losing current copy
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
